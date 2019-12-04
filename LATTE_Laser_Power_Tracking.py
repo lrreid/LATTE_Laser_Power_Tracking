e52ef9dba47256cb6d2eaec0b3facc3ad6d6038b
Lewis Reid <Lewis.Reid@Liverpool.ac.uk>
2019-12-04 16:35:09 +0000

    Plot update & button disabling
    Plotting updated so that axis lables appear, most recent 30 data points shown only and spacing of xticks controlled.
    
    Issue with not being able to stop the data being saved more than once has also been resolved.

--- LATTE_Laser_Power_Tracking.py
+++ LATTE_Laser_Power_Tracking.py
@@ -10,12 +10,9 @@ Script to input the power of the LATTE laser on a daily basis
 - Plots made to show the laser performance over time
 
 Still to be done:
-    - Warning for entries containing the same date and how this has to be delt with
     - Stop save button after first press
     - General tiding and comments
-    - Only show last x days of data, make xticks appropriate. Every 5 days?
     - Turn into execuitable?
-    - Plot lables not appearing
 """
 
 ################### Import functions ###################
@@ -25,7 +22,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter
 
-fsize = 12                                  # font size for axis lables, ticks and annotations
+fsize = 14                                  # font size for axis lables, ticks and annotations
 today = datetime.date.today().strftime('%d/%m/%Y').__str__()
 
 ################### Define functions for buttons ###################
@@ -47,32 +44,39 @@ def save():
     #fp.write("%s\t %s\t %s\t %s\t %s\t %s\n" % ("date", "Micra Power (mW)", "Regen energy (mJ)", "Powerlite 1 (W)", "Powerlite 2 (W)", "Full power (W)"))
     fp.write("%s\t %0.2f\t %0.2f\t %0.2f\t %0.2f\t %0.2f\n" % (date, Micra, Regen, Powerlite1, Powerlite2, Full_Power))
     fp.close()
-    Message = tk.Tk()      # Root object, creates blank window
-    tk.Label(Message, text="Data saved",bg="white",fg="black",font="none 10 bold") .grid(row=0, column=0, sticky=tk.W)
-    Message.mainloop()
+    Save_Data.config(state='disabled')      # Disable button after data is saved
 
 def plot():
     data     = np.loadtxt(open("LATTE_Laser_Power_History.txt"), skiprows=1)
     num2date = [datetime.datetime.strptime(str(int(data[k,0])), '%Y%m%d').date() for k in range(len(data[:,0]))]
     dates    = np.array(num2date, dtype='datetime64')
-
-    D_max = np.amax(dates)     # Find first data point for min
-    D_min = np.amin(dates)     # Find most recent data point for max
     Regen_target = np.array([2.0, 2.0])     # Target energy for regen is 2 mJ
+    
+    Regen_Ene = data[:,2]
+    PL1_Ene   = np.transpose(np.array([data[:,3]*100]))     # Puse energy in mJ for Powerlite 1
+    PL2_Ene   = np.transpose(np.array([data[:,4]*100]))     # Puse energy in mJ for Powerlite 2
+    Full_Ene  = np.transpose(np.array([data[:,5]*100]))     # Puse energy in mJ for Full power
+
+    if len(dates) > 30:
+        dates     = dates[len(dates)-30:len(dates)]
+        Regen_Ene = Regen_Ene[len(dates)-30:len(dates)]
+        PL1_Ene   = PL1_Ene[len(dates)-30:len(dates)]
+        PL2_Ene   = PL2_Ene[len(dates)-30:len(dates)]
+        Full_Ene  = Full_Ene[len(dates)-30:len(dates)]
+
+    D_max = np.amax(dates)+1     # Find first data point for min
+    D_min = np.amin(dates)       # Find most recent data point for max
     timespan = np.array([D_min, D_max])
-
-    PL1_Ene  = np.transpose(np.array([data[:,3]*100]))     # Puse energy in mJ for Powerlite 1
-    PL2_Ene  = np.transpose(np.array([data[:,4]*100]))     # Puse energy in mJ for Powerlite 2
-    Full_Ene = np.transpose(np.array([data[:,5]*100]))     # Puse energy in mJ for Full power
-
 
     fig, (ax1, ax2)  = plt.subplots(1, 2, figsize=(12,6), sharey=False)
     plt.subplot(121)
-    plt.plot(dates,data[:,2],'b-s',label="Regen energy")
+    plt.plot(dates,Regen_Ene,'b-s',label="Regen energy")
     plt.plot(timespan, Regen_target, 'r--',label="Target")
+    plt.xticks(np.arange(min(dates)-5, max(dates)+5, step=5))
     plt.yticks(np.arange(0, 4, step=0.5))
     plt.axis([D_min, D_max, 1, 3])         # set axes [xmin, xmax, ymin, ymax]
     plt.tick_params(labelsize=fsize)
+    ax1 = plt.gca()                             # Required for axis labels to appear
     ax1.set_xlabel('Date', fontsize=fsize)
     ax1.set_ylabel('Pulse energy (mJ)', fontsize=fsize)
     plt.title('Regen energy', fontsize=fsize)
@@ -85,9 +89,11 @@ def plot():
     plt.plot(dates,PL1_Ene,'b-s', label="Powerlite 1")
     plt.plot(dates,PL2_Ene,'g-s', label="Powerlite 2")
     plt.plot(dates,Full_Ene,'r-s', label="Full power")
+    plt.xticks(np.arange(min(dates)-5, max(dates)+5, step=5))
     plt.yticks(np.arange(0, 1200, step=100))
     plt.axis([D_min, D_max, 0, 800])         # set axes [xmin, xmax, ymin, ymax]
     plt.tick_params(labelsize=fsize)
+    ax2 = plt.gca()                             # Required for axis labels to appear
     ax2.set_xlabel('Date', fontsize=fsize)
     ax2.set_ylabel('Pulse energy (mJ)', fontsize=fsize)
     plt.title('Multipass energy', fontsize=fsize)
@@ -158,13 +164,18 @@ Full_Pow.grid(row=10,column=1,sticky=tk.W)
 window.grid_rowconfigure(11, minsize=20)         # Add white space before buttons
 
 # add a save button
-tk.Button(window, text="Save", width=6, command=save) .grid(row=12,column=0, sticky=tk.W)
+Save_Data = tk.Button(window, text="Save", width=6, command=save)
+Save_Data.grid(row=12,column=0, sticky=tk.W)
+Save_Data.config(state='normal')
+
 # add a button to plot measurements
-Do_Plots = tk.Button(window, text="Plot", width=6, command=plot) .grid(row=12,column=1, sticky=tk.W)
+Do_plot = tk.Button(window, text="Plot", width=6, command=plot)
+Do_plot.grid(row=12,column=1, sticky=tk.W)
 
 window.grid_rowconfigure(13, minsize=20)         # Add white space before buttons
 # add a button to exit program
-tk.Button(window, text="Exit", width=6, command=close_window) .grid(row=14,column=0, sticky=tk.W)
+Exit_button = tk.Button(window, text="Exit", width=6, command=close_window)
+Exit_button.grid(row=14,column=0, sticky=tk.W)
 
 # run the main loop
 window.mainloop()
--- txt_read_testing.py
+++ txt_read_testing.py
@@ -13,29 +13,39 @@ import matplotlib.pyplot as plt
 import datetime
 from matplotlib.dates import DateFormatter
 
-fsize = 12                                  # font size for axis lables, ticks and annotations
+fsize = 14                                  # font size for axis lables, ticks and annotations
 
-data     = np.loadtxt(open("LATTE_Laser_Power_History.txt"), skiprows=1)
+data     = np.loadtxt(open("LATTE_Laser_Power_History_big.txt"), skiprows=1)
 num2date = [datetime.datetime.strptime(str(int(data[k,0])), '%Y%m%d').date() for k in range(len(data[:,0]))]
 dates    = np.array(num2date, dtype='datetime64')
 
-D_max = np.amax(dates)     # Find first data point for min
-D_min = np.amin(dates)     # Find most recent data point for max
 Regen_target = np.array([2.0, 2.0])
+
+Regen_Ene = data[:,2]
+PL1_Ene   = np.transpose(np.array([data[:,3]*100]))     # Puse energy in mJ for Powerlite 1
+PL2_Ene   = np.transpose(np.array([data[:,4]*100]))     # Puse energy in mJ for Powerlite 2
+Full_Ene  = np.transpose(np.array([data[:,5]*100]))     # Puse energy in mJ for Full power
+
+if len(dates) > 30:
+    dates     = dates[len(dates)-30:len(dates)]
+    Regen_Ene = Regen_Ene[len(dates)-30:len(dates)]
+    PL1_Ene   = PL1_Ene[len(dates)-30:len(dates)]
+    PL2_Ene   = PL2_Ene[len(dates)-30:len(dates)]
+    Full_Ene  = Full_Ene[len(dates)-30:len(dates)]
+
+D_max = np.amax(dates)+1     # Find first data point for min
+D_min = np.amin(dates)       # Find most recent data point for max
 timespan = np.array([D_min, D_max])
-
-PL1_Ene  = np.transpose(np.array([data[:,3]*100]))     # Puse energy in mJ for Powerlite 1
-PL2_Ene  = np.transpose(np.array([data[:,4]*100]))     # Puse energy in mJ for Powerlite 2
-Full_Ene = np.transpose(np.array([data[:,5]*100]))     # Puse energy in mJ for Full power
-
 
 fig, (ax1, ax2)  = plt.subplots(1, 2, figsize=(12,6), sharey=False)
 plt.subplot(121)
-plt.plot(dates,data[:,2],'b-s',label="Regen energy")
+plt.plot(dates,Regen_Ene,'b-s',label="Regen energy")
 plt.plot(timespan, Regen_target, 'r--',label="Target")
+plt.xticks(np.arange(min(dates)-5, max(dates)+5, step=5))
 plt.yticks(np.arange(0, 4, step=0.5))
 plt.axis([D_min, D_max, 1, 3])         # set axes [xmin, xmax, ymin, ymax]
 plt.tick_params(labelsize=fsize)
+ax1 = plt.gca()                             # Required for axis labels to appear
 ax1.set_xlabel('Date', fontsize=fsize)
 ax1.set_ylabel('Pulse energy (mJ)', fontsize=fsize)
 plt.title('Regen energy', fontsize=fsize)
@@ -44,13 +54,16 @@ fig.autofmt_xdate()   # rotate and align the tick labels so they look better
 ax1.xaxis.set_major_formatter(DateFormatter("%d/%m"))
 plt.legend(bbox_to_anchor=(0.01, 0.20), loc='upper left', borderaxespad=0.)
 
+
 plt.subplot(122)
 plt.plot(dates,PL1_Ene,'b-s', label="Powerlite 1")
 plt.plot(dates,PL2_Ene,'g-s', label="Powerlite 2")
 plt.plot(dates,Full_Ene,'r-s', label="Full power")
+plt.xticks(np.arange(min(dates)-5, max(dates)+5, step=5))
 plt.yticks(np.arange(0, 1100, step=200))
 plt.axis([D_min, D_max, 0, 800])         # set axes [xmin, xmax, ymin, ymax]
 plt.tick_params(labelsize=fsize)
+ax2 = plt.gca()                             # Required for axis labels to appear
 ax2.set_xlabel('Date', fontsize=fsize)
 ax2.set_ylabel('Pulse energy (mJ)', fontsize=fsize)
 plt.title('Multipass energy', fontsize=fsize)
